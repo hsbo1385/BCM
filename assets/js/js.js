60979c257378341e282dd6e0bcc0fb2006a3e488
hsbo1385 <hsbo1385@aubih.edu>
2018-06-06 21:48:54 +0200

    fixes

--- assets/js/js.js
+++ assets/js/js.js
@@ -6,8 +6,8 @@ function scrollToSection(id){
 //scrolling for header
 $(window).scroll(function () {
     if ($(window).scrollTop() > 0) {
-        $(".navbar").addClass("scrolled");
+        $("nav.menu").addClass("scrolled");
     } else {
-        $(".navbar").removeClass("scrolled");
+        $("nav.menu").removeClass("scrolled");
     }
 });
