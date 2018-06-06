893e414cb2adbf95bfd2f2efce3509e49f83247d
hsbo1385 <hsbo1385@aubih.edu>
2018-06-06 21:27:59 +0200

    added new demo template for contact

--- includes/template/sections/section-contact.php
+++ includes/template/sections/section-contact.php
@@ -1,3 +1,42 @@
-<div id="section-<?=$section->get_id()?>" class="contact">
+<?php
+if(count($fields) > 0):
+?>
 
+<div id="section-<?=$section->get_id()?>" class="contact">
+    <div class="container">
+        <div class="row">
+        <?php
+            if($fields[$c]):
+            ?>
+            <div class="col-6">
+                <h2><?=$fields[$c]->get_content()?></h2>
+            </div>
+            <?php 
+            endif;
+            $c++;
+            if($fields[$c]):
+            ?>
+            <div class="col-6">
+                <p>
+                    <?=$fields[$c]->get_content()?>
+                </p>
+            </div>
+            <?php
+            endif;
+            $c++;
+            if($fields[$c]):
+            ?>
+            <div class="col-12">
+                <p>
+                    <?=$fields[$c]->get_content()?>
+                </p>
+            </div>
+            <?php
+            endif;
+            ?>
+        </div>
+    </div>
 </div>
+<?php 
+endif;
+?>
